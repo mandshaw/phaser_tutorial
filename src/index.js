15c51ed57c23edc69f2c6fe6c5ef981daaa4fdb1
Michael Shaw <michael.and.shaw@gmail.com>
2020-10-31 00:35:57 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+
+import GameScene from "./scenes/GameScene";
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
   width: 800,
   height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+    },
+  },
+  scene: [GameScene],
 };
 
-const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("logo", logoImg);
-}
-
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
+export default new Phaser.Game(config);
